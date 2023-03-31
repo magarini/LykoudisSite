f8496ee569951411026b9b01717766f5eef52a98
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-03-31 16:43:08 +0300

    burger menu fixed

---
+++ general.js
@@ -0,0 +1,8 @@
+function toggleBurger() {
+    var x = document.querySelector(".menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
