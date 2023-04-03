6380557232190f2e46f1e93189ce6a3ef258fe8b
Katerina Maganiari <k.maganiari@cleartechind.net>
2023-04-03 16:33:31 +0300

    burger menu in all pages & background color

--- general.js
+++ general.js
@@ -1,8 +1,13 @@
 function toggleBurger() {
     var x = document.querySelector(".menu");
-    if (x.style.display === "block") {
+    var navBar=document.querySelector(".burgerMenu");
+
+    if (x.style.display === "flex") {
       x.style.display = "none";
+      navBar.style.backgroundColor="transparent"
+
     } else {
-      x.style.display = "block";
+      x.style.display = "flex";
+      navBar.style.backgroundColor="#0E0E0E"
     }
   } 
